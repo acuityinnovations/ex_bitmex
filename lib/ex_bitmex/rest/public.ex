024436debf250048fcdfe84dd97fa9e2a766ea47
quan tran <quan@acuity.sg>
2021-07-07 14:55:38 +0700

    add reverse

--- lib/ex_bitmex/rest/public.ex
+++ lib/ex_bitmex/rest/public.ex
@@ -4,7 +4,7 @@ defmodule ExBitmex.PublicApi do
   def get_kline_data(instrument, interval, limit) do
     Rest.HTTPClient.non_auth_get(
       "/trade/bucketed",
-      %{symbol: instrument, binSize: interval, partial: true, count: limit}
+      %{symbol: instrument, binSize: interval, partial: true, count: limit, reverse: true}
     )
   end
 end
